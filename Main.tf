3bb955a4c9263fbe7d4681d418eb823aef1e724c
satyanarayansinghindia <61449509+satyanarayansinghindia@users.noreply.github.com>
2021-11-27 23:13:42 +0530

    Create Main.tf

---
+++ Main.tf
@@ -0,0 +1,40 @@
+provider "google" {
+project = "skillful-air-333110"
+region = "us-central1"
+}
+
+variable "machine" {
+type = string
+default = "f1-micro"
+}
+variable "project" {
+type = string
+}
+
+resource "google_compute_instance" "myvm01" {
+name = "myfirstvm"
+machine_type = var.machine
+zone = "us-central1-a"
+network_interface {
+network = "default"
+}
+boot_disk {
+initialize_params {
+image = "debian-cloud/debian-10"
+}
+}
+}
+
+resource "google_compute_instance" "myvm02" {
+name = "mysecondvm"
+machine_type = var. machine
+zone = "us-central1-a"
+network_interface {
+network = "default"
+}
+boot_disk {
+initialize_params {
+image = "rhel-cloud/rhel-7"
+}
+}
+}
